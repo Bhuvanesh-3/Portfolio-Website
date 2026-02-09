577009acb98e65e1d987c4853f24ae2fb6276c28
Bhuvanesh-3 <bhuvaneshbabuk@gmail.com>
2026-02-09 12:37:03 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,7 +1,7 @@
-function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
-}
-
-document.getElementById("darkToggle").addEventListener("click", () => {
-  document.body.classList.toggle("dark");
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
 });
